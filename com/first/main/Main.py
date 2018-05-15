765e04e521452dfe667ed8c44c5cca09be2d79c6
sarthak7295 <sarthak.rawat@yahoo.com>
2018-05-15 21:35:06 +0530

    initial commit for flask

---
+++ com/first/main/Main.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+
+@app.route('/hello')
+def hello_world2():
+    return 'hello world2'
+
+
+@app.route('/hello/<value>')
+def variable_test(value):
+    return 'hello world '+value
+
+
+@app.route('/hellonumber/<int:post_id>')
+def show_blog(post_id):
+    return 'Blog Number %d' % post_id
+
+
+@app.route('/helloheading')
+def index():
+    return render_template('hello.html', name='sarthak')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
