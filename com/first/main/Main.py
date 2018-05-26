f5c385fa97795c6d800a9d67b3c93226ed58e8c8
sarthak <sarthak.rawat@yahoo.com>
2018-05-26 22:06:33 +0530

    upload file process added

--- com/first/main/Main.py
+++ com/first/main/Main.py
@@ -1,8 +1,12 @@
 
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, render_template,redirect, url_for
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/home/sarthak/FlaskPics'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -28,6 +32,36 @@ def show_blog(post_id):
 def index():
     return render_template('hello.html', name='sarthak')
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'done upload'#redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug='true',host='0.0.0.0' ,port=8080)
